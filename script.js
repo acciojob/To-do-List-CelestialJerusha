ef4c8839b833caa87ee1cfd34c9035e6d21e9587
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-10-16 22:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ let list = document.getElementById("todoList");
 function add(){
 	let text = document.getElementById("newTodoInput").value;
 	let li = document.createElement("li");
-	li = text;
-	list.innerHTML += li;
+	li.innerHTML = text;
+	list.appendChild(li);
 }
